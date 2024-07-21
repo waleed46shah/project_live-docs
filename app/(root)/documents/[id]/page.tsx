6b2b816cbb49b48bf9c40a5be5b130169d9acee8
waleed46shah <waleed46shah@gmail.com>
2024-07-21 15:14:54 +0500

    Test clerk auth components

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,12 +1,21 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const page = () => {
   return (
     <div>
       <Header>
-        <p className="text-white">Test</p>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">This is a fake document title</p>
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>
