f8fcc2cce563a7d94c152402dde8560b155666fe
waleed46shah <waleed46shah@gmail.com>
2024-07-21 13:55:42 +0500

    Fix layout and page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
-import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
-
+import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "Live Docs",
+  description: "A live documentation tool for your team",
+};
 
 export default function RootLayout({
   children,
@@ -17,12 +22,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head />
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
+        className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
       >
-        ...
+        {children}
       </body>
     </html>
   );
