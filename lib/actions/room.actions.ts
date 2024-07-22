ef4125aed8f19bdec46135a50dd2b870313dd584
waleed46shah <waleed46shah@gmail.com>
2024-07-22 11:56:38 +0500

    Create getDocument action

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -24,7 +24,7 @@ export const createDocument = async ({
     const room = await liveblocks.createRoom(roomId, {
       metadata,
       usersAccesses,
-      defaultAccesses: [],
+      defaultAccesses: ["room:write"],
     });
 
     revalidatePath("/");
@@ -32,5 +32,27 @@ export const createDocument = async ({
     return parseStringify(room);
   } catch (error) {
     console.log(`Error happened while creating room: ${error}`);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+    if (!hasAccess) {
+      throw new Error("You don't have access to this document");
+    } else {
+      parseStringify(room);
+    }
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while getting room: ${error}`);
   }
 };
